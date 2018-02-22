3d407ca02156c13b99c09bfcb84548ae6d1283d6
vvsvintsitsky <v.v.svintsitsky@gmail.com>
2018-02-22 11:41:50 +0300

    completed fourth task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var coefficientArray = equation.split(" ");
+  
+  var a = parseInt(coefficientArray[0]);
+  var b = parseInt(coefficientArray[4]) * (coefficientArray[3] == '+' ? 1 : -1) / a;
+  var c = parseInt(coefficientArray[8]) * (coefficientArray[7] == '+' ? 1 : -1) / a;
+
+  var discriminantSqrt = Math.sqrt(b*b - 4*c);
+
+  x1 = ((b*(-1) + discriminantSqrt) / 2);
+  x2 = ((b*(-1) - discriminantSqrt) / 2);
+
+  if(x1 < x2) {
+    return [x1, x2];
+  } else {
+    return [x2, x1];
+  }
 }
